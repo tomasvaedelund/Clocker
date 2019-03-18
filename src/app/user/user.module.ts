3b6f7047f557ae72cab24e647ba78698c8249b26
Vaedelund <tomas.vaedelund@abetong.se>
2019-03-18 09:55:28 +0100

    Added user/edit page

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -6,9 +6,10 @@ import { UserRoutingModule } from './user-routing.module';
 import { UserService } from './user.service';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { EditComponent } from './edit/edit.component';
 
 @NgModule({
-  declarations: [LoginComponent, RegisterComponent],
+  declarations: [LoginComponent, RegisterComponent, EditComponent],
   imports: [CommonModule, UserRoutingModule, ReactiveFormsModule],
   providers: [UserService]
 })
