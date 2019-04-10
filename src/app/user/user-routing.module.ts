cbfc03b38b448b24f94002ff4c94860844a1a546
Vaedelund <tomas.vaedelund@abetong.se>
2019-04-10 15:57:43 +0200

    Wrong component caused an arror

--- src/app/user/user-routing.module.ts
+++ src/app/user/user-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
-import { EditComponent } from '../clocker/edit/edit.component';
+import { EditComponent } from './edit/edit.component';
 
 const routes: Routes = [
   {
